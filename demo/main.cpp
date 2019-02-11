ba4eaa4ffc2f2f52040d957e84c5b64cb1adae0f
Conor Damery <mike@alonica.net>
2019-02-11 23:23:40 +0100

    Fixed cmake script

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,8 +1,12 @@
-#include <ove/math/interpolation.hpp>
+#include <iostream>
+#include <ove/math/vec.hpp>
 
 int main()
 {
-    interpolation i;
-    i.print();
-	return 0;
+    math::vec2f v1(2, 2);
+    math::vec2f v2(4, 4);
+    auto v3 = v1 + v2;
+    std::cout << v3.x << ", " << v3.y << std::endl;
+
+    return 0;
 }
